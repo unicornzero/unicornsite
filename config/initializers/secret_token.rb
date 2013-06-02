b522b6fd03a52ba8a6f2481a55585a45a64b7a4d
unicornzero <rylee.k@gmail.com>
2013-06-02 10:51:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unicornsite::Application.config.secret_token = '49f79656993f2a4e9e7b17f18f9c801ed086c8b829f00627d8ea7daf9cb54d09087215403f83576d78907dc23dc6daf93c506ce603128b115415ae8d3c659942'
